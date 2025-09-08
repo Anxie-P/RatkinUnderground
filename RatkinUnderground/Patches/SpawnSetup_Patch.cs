fa31acfe38c7cad892cc87a35e86112c12c0b030
BA4413 <2073600373@qq.com>
2025-09-08 14:46:00 +0800

    - 修复 钻进进地图自选生成位置

--- RatkinUnderground/Patches/SpawnSetup_Patch.cs
+++ RatkinUnderground/Patches/SpawnSetup_Patch.cs
@@ -4,6 +4,7 @@ using RimWorld;
 using RimWorld.Planet;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Verse;
 
@@ -18,24 +19,82 @@ public static class Patch_CaravanEnter
         {
             Log.Message($"[RKU] Caravan {caravan.Label} 捕获，准备替换为 RKU_DrillingVehicle。");
 
-            // 获取生成点
-            IntVec3 spawnPos = spawnCellGetter(caravan.PawnsListForReading.FirstOrDefault());
-
-            // 生成钻机建筑
-            var vehicle = (RKU_TunnelHiveSpawner)ThingMaker.MakeThing(DefOfs.RKU_TunnelHiveSpawner);
-            vehicle.hitPoints = rkuCaravan.hitPoints;
-            vehicle.faction = rkuCaravan.Faction;
-
-            // 所有pawn移动到钻机
-            foreach (var p in caravan.PawnsListForReading.ToList())
+            if (map == null)
             {
-                vehicle.GetDirectlyHeldThings().TryAddOrTransfer(p);
-                rkuCaravan.RemovePawn(p);
+                Log.Error("[RKU] 目标 map 为 null");
+                return true;
             }
-            GenSpawn.Spawn(vehicle, spawnPos, map);
-            // 移除 caravan
-            if (!caravan.Destroyed)
-                caravan.Destroy();
+
+            // 移动中触发的事件
+            if (!rkuCaravan.IsArrived()
+                && rkuCaravan.pather.MovingNow)
+            {
+                return true;
+            }
+            
+            CameraJumper.TryJump(map.Center, map);
+
+            Find.Targeter.BeginTargeting(
+                new TargetingParameters
+                {
+                    canTargetLocations = true,
+                    canTargetSelf = false,
+                    canTargetPawns = false,
+                    canTargetBuildings = false
+                },
+                delegate (LocalTargetInfo target)
+                {
+                    try
+                    {
+                        // 获取生成点
+                        // IntVec3 spawnPos = spawnCellGetter(caravan.PawnsListForReading.FirstOrDefault());
+                        IntVec3 spawnPos = target.Cell;
+                        Log.Message($"选择位置:{target.Cell}");
+                        // 生成钻机建筑
+                        var vehicle = (RKU_TunnelHiveSpawner)ThingMaker.MakeThing(DefOfs.RKU_TunnelHiveSpawner);
+                        vehicle.hitPoints = rkuCaravan.hitPoints;
+                        vehicle.faction = rkuCaravan.Faction;
+
+                        // 所有pawn移动到钻机
+                        foreach (var p in caravan.PawnsListForReading.ToList())
+                        {
+                            try
+                            {
+                                vehicle.GetDirectlyHeldThings().TryAddOrTransfer(p);
+                                rkuCaravan.RemovePawn(p);
+                            }
+                            catch (Exception e)
+                            {
+                                Log.Message($"遍历报错：{e}");
+                            }
+                            
+                        }
+                        GenSpawn.Spawn(vehicle, spawnPos, map);
+                        // 移除 caravan
+                        if (!caravan.Destroyed)
+                            caravan.Destroy();
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Message($"delegate报错：{e}");
+                    }
+                },
+                null,
+                (LocalTargetInfo target) =>
+                {
+                    try
+                    {
+                        if (!target.IsValid) return false;
+                        if (!target.Cell.InBounds(map)) return false;
+                        if (!target.Cell.Standable(map)) return false;
+                        return true;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+                );
 
             return false;
         }
