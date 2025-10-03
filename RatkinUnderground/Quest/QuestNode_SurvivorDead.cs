6ddb2e5efe5609a2eee5cbb71964c2df5c2834fa
BA4413 <2073600373@qq.com>
2025-10-03 08:57:14 +0800

    - 修复 幸存者生成在墙里的问题

---
+++ RatkinUnderground/Quest/QuestNode_SurvivorDead.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using RimWorld.QuestGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.Grammar;
+
+namespace RatkinUnderground
+{
+    
+}
---
+++ RatkinUnderground/Quest/QuestPart_RKU_GuerrillaSurvivor.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using RimWorld.QuestGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.Noise;
+
+namespace RatkinUnderground
+{
+    public class QuestPart_RKU_GuerrillaSurvivor : QuestPart
+    {
+        public string inSignal;
+
+        public override void Notify_QuestSignalReceived(Signal signal)
+        {
+            base.Notify_QuestSignalReceived(signal);
+            if (signal.tag == inSignal)
+            {
+                Log.Message($"[RKU] QuestPart 收到失败信号 {inSignal}，将 quest 结束为 Fail。");
+                if (this.quest != null)
+                {
+                    this.quest.End(QuestEndOutcome.Fail);
+                }
+            }
+        }
+    }
+}
